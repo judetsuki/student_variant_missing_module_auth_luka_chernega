0a4bd10688816b59d58141abd415171ae7e3669b
judetsuki <Luka9609@gmail.com>
2026-04-17 18:03:46 +0300

    added forms

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('create/', views.product_create, name='product_create'),
     path('<int:pk>/update/', views.product_update, name='product_update'),
     path('<int:pk>/delete/', views.product_delete, name='product_delete'),
+    
+    path('orders/', views.order_list, name='order_list'),
+    path('<int:pk>/delete/', views.order_delete, name='order_delete'),
+    path('<int:pk>/update/', views.order_update, name='order_update'),
+    path('orders/create/', views.order_create, name='order_create')
 ]
--- products/views.py
+++ products/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
 from django.core.paginator import Paginator
-from .models import Product, Supplier
-from .forms import ProductForm
+from .models import Product, Supplier, Order, OrderStatus
+from .forms import ProductForm, OrderForm
 
 
 def get_user_role(user):
@@ -152,3 +152,106 @@ def product_delete(request, pk):
         'product': product,
         'user_role': get_user_role(request.user)
     })
+
+def order_list(request):
+    user_role = get_user_role(request.user) if request.user.is_authenticated else 'guest'
+    
+    orders = Order.objects.select_related('status').all()
+
+    search_query = request.GET.get('search', '')
+    status_filter = request.GET.get('status', '')
+
+    if user_role == 'admin':
+        if search_query:
+            orders = orders.filter(
+                Q(status__status__icontains=search_query) | 
+                Q(id__icontains=search_query) 
+            )
+
+        
+        if status_filter:
+            orders = orders.filter(status_id=status_filter)
+
+    
+    paginator = Paginator(orders.order_by('-id'), 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    
+    context = {
+        'page_obj': page_obj,
+        'user_role': user_role,
+        'statuses': OrderStatus.objects.all(), 
+        'search_query': search_query,
+        'status_filter': status_filter,
+    }
+
+    return render(request, 'products/order_list.html', context)
+
+
+@login_required
+def order_create(request):
+    if not request.user.is_superuser:
+        messages.error(request, 'У вас нет прав на это действие')
+        return redirect('products:order_list')
+    
+    if request.method == 'POST':
+        form = OrderForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Заказ создан')
+            return redirect('products:order_list')
+    else:
+        form = OrderForm()
+
+    return render(request, 'products/order_form.html', {
+        'form': form,
+        'title': 'Добавить заказ',
+        'user_role': get_user_role(request.user)
+    })
+
+
+@login_required
+def order_update(request, pk):
+    if not request.user.is_superuser:
+        messages.error(request, 'У вас нет прав для выполнения этого действия.')
+        return redirect('products:order_list')
+
+    order = get_object_or_404(Order, pk=pk)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, request.FILES, instance=order)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'заказ обновлен.')
+            return redirect('products:order_list')
+    else:
+        form = OrderForm(instance=order)
+
+    return render(request, 'products/order_form.html', {
+        'form': form,
+        'order': order,
+        'title': 'Редактировать заказ',
+        'user_role': get_user_role(request.user)
+    })
+
+@login_required
+def order_delete(request, pk):
+    if not request.user.is_superuser:
+        messages.error(request, 'У вас нет прав для выполнения этого действия.')
+        return redirect('products:order_list')
+
+    order = get_object_or_404(Order, pk=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        messages.success(request, 'заказ удален.')
+        return redirect('products:order_list')
+
+    return render(request, 'products/order_confirm_delete.html', {
+        'order': order,
+        'user_role': get_user_role(request.user)
+    })
+
+
+

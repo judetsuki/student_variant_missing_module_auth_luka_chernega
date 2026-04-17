4b902370f49d11c9436575913bac58feae808c9d
judetsuki <Luka9609@gmail.com>
2026-04-17 17:03:50 +0300

    fixed models

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from decimal import Decimal
-
+from django.conf import settings
 
 class Category(models.Model):
     """Категория товара"""
@@ -59,9 +59,9 @@ class Order(models.Model):
     order_number = models.CharField(max_length=20,unique=True, verbose_name="Номер заказа")
     order_date = models.DateField(verbose_name="Дата заказа")
     delivery_date = models.DateField(verbose_name="Дата доставки")
-    customer_id = models.ForeignKey('User',on_delete=models.CASCADE, verbose_name='Клиент')
+    customer_id = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, verbose_name='Клиент')
     pickup_point_id = models.ForeignKey('PickupPoint', on_delete=models.CASCADE,verbose_name='Адрес пункта доставки')
-    status = models.ForeignKey('Status',on_delete=models.CASCADE, verbose_name="Статус заказа")
+    status = models.ForeignKey('OrderStatus',on_delete=models.CASCADE, verbose_name="Статус заказа")
 
 
 class OrderItem(models.Model):
@@ -70,12 +70,17 @@ class OrderItem(models.Model):
     order_id = models.ForeignKey('Order', on_delete=models.CASCADE,verbose_name='Заказ')
     product_id = models.ForeignKey('Product', on_delete=models.CASCADE,verbose_name='Товар')
 
-class OrderStatus(models.Model):
-        STATUS_CHOICES = [
+class OrderStatus(models.Model):    
+    STATUS_CHOICES = [
         ('Завершен', 'Завершен'),
         ('Новый', 'Новый')
-    ]
-        status = models.CharField(choices=STATUS_CHOICES,max_length=30,verbose_name='Статус заказа')
+    ]  
+    status = models.CharField(choices=STATUS_CHOICES,max_length=30,verbose_name='Статус заказа')
+
+class PickupPoint(models.Model):
+    address = models.CharField(max_length=100,verbose_name='Адрес пункта выдачи')
+
+
 
 
 
